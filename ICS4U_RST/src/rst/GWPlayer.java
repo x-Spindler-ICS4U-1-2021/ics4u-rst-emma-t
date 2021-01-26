2e458d69c482b8827c563296000a276f002de2bd
emma_ <emma_@LAPTOP-MKHLSV1T>
2021-01-26 01:45:04 -0500

    I decided to change the characteristics array to an arraylist so that the algorithm (to make sure characters aren't repeated) works properly. Still needs to be cleaned up quite a bit, too much code is repeated, not used, or just one line comments at the moment

--- ICS4U_RST/src/rst/GWPlayer.java
+++ ICS4U_RST/src/rst/GWPlayer.java
@@ -1,6 +1,6 @@
 package rst;
 
-import java.util.Optional;
+import java.util.ArrayList;
 
 public class GWPlayer {
 	
@@ -12,36 +12,67 @@ public class GWPlayer {
 			"YBLS", "BNSL", "BNSS", "BNML", "BNMS", "BNLL", "BNLS", "BFSL", "BFSS", "BFML", "BFMS", "BFLL", "BFLS",
 			"BBSL", "BBSS", "BBML", "BBMS", "BBLL", "BBLS"};
 	private String characters[] = new String [12];
+	
+	//private ArrayList arrayList = new ArrayList(); 
+	private ArrayList<String> arrayList = new ArrayList<String>();
+	
 	private Character charactersCharacteristics[] = new Character [12];
 	private boolean isCorrectGuess = false;
 	
 	public GWPlayer() {
-		String characteristicsCopy [] = new String [54];
-	
+		
 		for(int z = 0; z < 54; z++) {
-			characteristicsCopy[z] = characteristics[z];
+			arrayList.add(characteristics[z]);
 		}
+//		arrayList.add("RNSL", "RNSS", "RNML", "RNMS", "RNLL", "RNLS", "RFSL", "RFSS", "RFML", 
+//				"RFMS", "RFLL", "RFLS", "RBSL", "RBSS", "RBML", "RBMS", "RBLL", "RBLS", "YNSL", "YNSS", "YNML", "YNMS",
+//				"YNLL", "YNLS", "YFSL", "YFSS", "YFML", "YFMS", "YFLL", "YFLS", "YBSL", "YBSS", "YBML", "YBMS", "YBLL",
+//				"YBLS", "BNSL", "BNSS", "BNML", "BNMS", "BNLL", "BNLS", "BFSL", "BFSS", "BFML", "BFMS", "BFLL", "BFLS",
+//				"BBSL", "BBSS", "BBML", "BBMS", "BBLL", "BBLS");
 		
 		int value;
 		for(int i = 0; i < 12; i++) {
-			value = (int)(Math.random()*characteristicsCopy.length);
+			value = (int)(Math.random()*arrayList.size());
 			
-			//charactersCharacteristics[i] = new Character(characteristicsCopy[value]);
-			characters[i] = characteristicsCopy[value];
+			characters[i] = arrayList.get(value);
 			
-			String temp [] = new String [characteristicsCopy.length-1];
-			
-			for (int x = 0, y = 0; x < characteristicsCopy.length; x++) { 
-				
-				if (x == value) { 
-					continue; 
-				} 
-				
-				temp[y++] = characteristicsCopy[x]; 	
-			} 
-			
-			
+			arrayList.remove(value);
 		}
+		
+//		String characteristicsCopy [] = new String [54];
+//	
+//		for(int z = 0; z < 54; z++) {
+//			characteristicsCopy[z] = characteristics[z];
+//		}
+//		
+//		int value;
+//		for(int i = 0; i < 12; i++) {
+//			value = (int)(Math.random()*characteristicsCopy.length);
+//			
+//			//charactersCharacteristics[i] = new Character(characteristicsCopy[value]);
+//			characters[i] = characteristicsCopy[value];
+//			
+//			String temp [] = new String [characteristicsCopy.length-1];
+//			
+//			
+//			
+//			for (int x = 0, y = 0; x < characteristicsCopy.length; x++) { 
+//				
+//				if (x == value) { 
+//					continue; 
+//				} 
+//				
+//				temp[y++] = characteristicsCopy[x]; 
+//				
+//				for(int z = 0; z < temp.length; z++) {
+//					characteristicsCopy[z] = temp[z];
+//				}
+//				
+//				
+//			} 
+//			
+//			
+//		}
 		
 	}
 	
